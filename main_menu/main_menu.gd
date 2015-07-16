7d97b0a4d40c35197d7b85fc9f64eb1930a088be
HER0_01 <aconrad103@gmail.com>
2015-07-16 01:53:59 -0500

    Import room backgrounds, main menu improvements, add portcullis scene.

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -2,9 +2,27 @@
 extends TextureFrame
 
 func _ready():
+	if get_node("..").is_in_group("room"):
+		# Is a pause menu
+		hide()
+		get_node("Menu/New").hide()
+		get_node("Menu/Continu").show()
+		get_node("Menu/Save").show()
+
+func _on_New_pressed():
+	get_node("/root/Music").fade_toggle()
+	get_node("/root/tree_switcher").goto_tree("res://rooms/portcullis/portcullis.xscn")
+
+func _on_Continue_pressed():
 	pass
 
-func _on_Start_pressed():
+func _on_Save_pressed():
+	pass
+
+func _on_Load_pressed():
+	pass
+
+func _on_About_pressed():
 	pass
 
 func _on_Quit_pressed():
