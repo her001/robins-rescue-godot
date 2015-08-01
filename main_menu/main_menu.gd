0faf64ee019e17be0c13dcd59fe36d7df8a88b54
HER0_01 <aconrad103@gmail.com>
2015-08-01 14:45:04 -0500

    Impliment basic pause menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,6 +1,8 @@
 
 extends TextureFrame
 
+var pause_button_pressed
+
 func _ready():
 	if get_node("..").is_in_group("room"):
 		# Is a pause menu
@@ -8,6 +10,8 @@ func _ready():
 		get_node("./Menu/New").hide()
 		get_node("./Menu/Continue").show()
 		get_node("./Menu/Save").show()
+		
+		set_process_input(true)
 	else:
 		preload("res://characters/marian/marian.xscn")
 
@@ -16,7 +20,7 @@ func _on_New_pressed():
 	get_node("/root/tree_switcher").goto_tree("res://rooms/portcullis/portcullis.xscn")
 
 func _on_Continue_pressed():
-	pass
+	pause_toggle()
 
 func _on_Save_pressed():
 	pass
@@ -29,3 +33,17 @@ func _on_About_pressed():
 
 func _on_Quit_pressed():
 	OS.get_main_loop().quit()
+
+func pause_toggle():
+	if get_tree().is_paused():
+		get_tree().set_pause(false)
+		hide()
+	else:
+		get_tree().set_pause(true)
+		show()
+	get_node("/root/Music").fade_toggle()
+
+func _input(ev):
+	if ev.is_action("pause") && ev.is_pressed():
+		get_tree().set_input_as_handled()
+		pause_toggle()
