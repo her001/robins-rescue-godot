0f389cb663f843e777b4670eda4ab8c391354d7c
HER0_01 <aconrad103@gmail.com>
2015-08-02 11:35:35 -0500

    Prevent audio track being swapped if switching too quickly

--- audio/music.gd
+++ audio/music.gd
@@ -4,35 +4,57 @@ extends StreamPlayer
 var menu_music
 var game_music
 var fade_to_music
-var stream
+var track
+
+const MENU_TRACK = 0
+const GAME_TRACK = 1
 
 func _ready():
 	menu_music = "res://audio/music/thatched_villagers.ogg"
 	game_music = "res://audio/music/pippin_the_hunchback.ogg"
 	fade_to_music = ""
-	stream = get_stream()
+	track = MENU_TRACK
 
 func fade_toggle():
-	if stream.get_path()==menu_music:
-		fade_to(game_music)
+	if track == MENU_TRACK:
+		track = GAME_TRACK
+		_fade_to(game_music)
 	else:
-		fade_to(menu_music)
+		track = MENU_TRACK
+		_fade_to(menu_music)
 
-func fade_to(new_track):
+func toggle_track():
+	if track == MENU_TRACK:
+		track = GAME_TRACK
+		_switch_to(game_music)
+	else:
+		track = MENU_TRACK
+		_switch_to(menu_music)
+
+func fade_new_music(new_track):
+	_set_game_music(new_track)
+	if track == GAME_TRACK:
+		_fade_to(new_track)
+
+func switch_new_music(new_track):
+	_set_game_music(new_track)
+	if track == GAME_TRACK:
+		_switch_to(new_track)
+
+func _fade_to(new_track):
 	get_child(0).stop_all()
-	get_child(0).play("fade to")
+	get_child(0).play("fade_to")
 	fade_to_music = new_track
 
-func switch_to(new_track):
+func _switch_to(new_track):
 	get_child(0).stop_all()
 	set_stream( load(new_track) )
 	set_volume(1)
 	play()
-	stream = get_stream()
 
-func set_game_music(track):
+func _set_game_music(track):
 	game_music = track
 
 func _switch_faded():
 	get_child(0).stop_all()
-	switch_to(fade_to_music)
+	_switch_to(fade_to_music)
