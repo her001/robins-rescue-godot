c26a7470a4ad834d5a30301ebd161a9252b3c807
HER0_01 <aconrad103@gmail.com>
2015-08-02 22:13:10 -0500

    Add some cursor switching logic

--- helpers/cursor/cursor.gd
+++ helpers/cursor/cursor.gd
@@ -1,9 +1,25 @@
 
 extends AnimatedSprite
 
+var base_cursor
+var is_alt_cursor
+
 func _ready():
+	base_cursor = load("res://helpers/cursor/standard_cursor.xml")
+	is_alt_cursor = false
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	set_process(true)
+
+func set_alt_cursor(sprite_frames):
+	if not is_alt_cursor:
+		set_sprite_frames(load(sprite_frames))
+		is_alt_cursor = true
+
+func set_base_cursor():
+	if is_alt_cursor:
+		set_sprite_frames(base_cursor)
+		is_alt_cursor = false
 
 func hovering():
 	set_frame(1)
