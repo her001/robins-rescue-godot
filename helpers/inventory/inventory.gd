2c54af7de2f5a55af933119fc2d830b7274ed902
HER0_01 <aconrad103@gmail.com>
2015-08-11 16:06:24 -0500

    Add cursor switching in inventory

--- helpers/inventory/inventory.gd
+++ helpers/inventory/inventory.gd
@@ -1,9 +1,18 @@
 
 extends TextureFrame
 
+var using_item
+var active_item
+
 func _ready():
 	hide()
+	using_item = false
+	active_item = ""
+	
 	set_process_input(true)
+
+func add_item():
+	pass
 
 func toggle_show():
 	var anim = get_node("AnimationPlayer")
@@ -14,10 +23,19 @@ func toggle_show():
 		anim.play("slide_down")
 
 func _input(ev):
-	if ev.is_action("inventory") && ev.is_pressed():
+	if ev.is_action("inventory") && ev.is_pressed() && not using_item:
 		get_tree().set_input_as_handled()
 		toggle_show()
-	
+	elif ev.is_action("inventory") && ev.is_pressed() && using_item:
+		get_tree().set_input_as_handled()
+		get_node("../Cursor").set_base_cursor()
 
 func _on_Close_pressed():
 	toggle_show()
+	get_node("../Cursor").stop_hovering()
+
+func _on_button_mouse_enter():
+	get_node("../Cursor").hovering()
+
+func _on_button_mouse_exit():
+	get_node("../Cursor").stop_hovering()
