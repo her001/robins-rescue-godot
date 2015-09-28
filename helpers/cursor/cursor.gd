021d539e885e6fd14d5d9c8e50c1453105a8d2f9
HER0_01 <aconrad103@gmail.com>
2015-09-27 23:18:16 -0500

    New cursor script (unused for now)

--- helpers/cursor/cursor.gd
+++ helpers/cursor/cursor.gd
@@ -1,27 +1,46 @@
 
-extends AnimatedSprite
+extends Node
 
 var base_cursor
+var base_cursor_hover
+var alt_cursor
+var alt_cursor_hover
 var is_alt_cursor
+var is_hovering
 
 func _ready():
-	base_cursor = load("res://helpers/cursor/standard_cursor.xml")
+	base_cursor = load("helpers/cursor/cursor_01.png")
+	base_cursor_hover = load("helpers/cursor/cursor_02.png")
 	is_alt_cursor = false
+	is_hovering = false
 	
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	#Input.set_custom_mouse_cursor(base_cursor, Vector2(32,32))
 
-func set_alt_cursor(sprite_frames):
-	if not is_alt_cursor:
-		set_sprite_frames(load(sprite_frames))
-		is_alt_cursor = true
+func set_alt_cursor(cursor, hover):
+	is_alt_cursor = true
+	alt_cursor = cursor
+	alt_cursor_hover = hover
+	if is_hovering:
+		hovering()
+	else:
+		stop_hovering()
 
 func set_base_cursor():
 	if is_alt_cursor:
-		set_sprite_frames(base_cursor)
 		is_alt_cursor = false
+		if is_hovering:
+			hovering()
+		else:
+			stop_hovering()
 
 func hovering():
-	set_frame(1)
+	if is_alt_cursor:
+		Input.set_custom_mouse_cursor(alt_cursor_hover, Vector2(32,32))
+	else:
+		Input.set_custom_mouse_cursor(base_cursor_hover, Vector2(32,32))
 
 func stop_hovering():
-	set_frame(0)
+	if is_alt_cursor:
+		Input.set_custom_mouse_cursor(alt_cursor, Vector2(32,32))
+	else:
+		Input.set_custom_mouse_cursor(base_cursor, Vector2(32,32))
